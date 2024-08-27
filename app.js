628392c4af61cdb3f8451c2cf2ab494bc24eed28
abdunazaarov <abduraximabdunazarov1@gmail.com>
2024-08-27 05:48:50 +0500

    month4 lesson2 homework

---
+++ app.js
@@ -0,0 +1,190 @@
+// 1 - Masala
+
+// {
+//     let a = 5
+
+//     if (a > 0){
+//         a += 1
+//         console.log(a)
+//     }
+//     else{
+//         console.log(a)
+//     }
+// }
+
+
+// 2 - Masala
+
+// {
+//     let a = 0
+
+//     if (a > 0){
+//         a += 1
+//         console.log(a)
+//     }
+//     else{
+//         a -= 2
+//         console.log(a)
+//     }
+// }
+
+// 3 - Masala
+
+// {
+//     let a = 0
+
+//     if (a > 0){
+//         a += 1
+//         console.log(a)
+//     }
+//     else if(a < 0){
+//         a -= 2
+//         console.log(a)
+//     }
+//     else{
+//         a = 10
+//         console.log(a)
+//     }
+// }
+
+// 4 - Masala
+
+// {
+//     let a = 5
+//     let b = 2
+//     let c = -1
+//     let count = 0
+
+//     if(a > 0){
+//         count += 1
+//     }
+//     if(b > 0){
+//         count += 1
+//     }
+//     if(c > 0){
+//         count += 1
+//     }
+
+//     console.log(`${count} ta musbat son bor.`)
+// }
+
+// 5 - Masala
+
+// {
+//     {
+//         let a = 5
+//         let b = 2
+//         let c = -1
+//         let countPlus = 0
+//         let countMinus = 0
+    
+//         if(a > 0){
+//             countPlus += 1
+//         }
+//         else if(a < 0){
+//             countMinus += 1
+//         }
+//         if(b > 0){
+//             countPlus += 1
+//         }
+//         else if(b < 0){
+//             countMinus += 1
+//         }
+//         if(c > 0){
+//             countPlus += 1
+//         }
+//         else if(c < 0){
+//             countMinus += 1
+//         }
+    
+//         console.log(`${countPlus} ta musbat ${countMinus} ta manfiy son bor.`)
+//     }
+// }
+
+// 6 - Masala
+
+// {
+//     let a = 4
+//     let b = 6
+
+//     if(a > b){
+//         console.log(`${a} son ${b} sondan katta`)
+//     }
+//     else if(b > a){
+//         console.log(`${b} son ${a} sondan katta.`)
+//     }
+//     else{
+//         console.log(`Ikki son ham teng`)
+//     }
+// }
+
+// 7 - Masala
+
+// {
+//     let a = 10
+//     let b = 20
+
+//     if(a < b){
+//         console.log(1)
+//     }
+//     else if(b < a){
+//         console.log(2)
+//     }
+//     else{
+//         console.log("Ikki son teng.")
+//     }
+// }
+
+// 8 - Masala
+
+// {
+//     let a = 10
+//     let b = 20
+
+//     if(a > b){
+//         console.log(a, b)
+//     }
+//     else if(b > a){
+//         console.log(b, a)
+//     }
+//     else{
+//         console.log("Ikki son teng.")
+//     }
+// }
+
+// 9 - Masala
+
+// {
+//     let a = 18.2
+//     let b = 13.75
+//     let c = null
+
+//     console.log(`a = ${a} va b = ${b}`)
+
+//     if (a > b){
+//         c = a
+//         a = b
+//         b = c
+//     }
+
+//     console.log(`a = ${a} va b = ${b}`)
+// }
+
+// 10 - Masala
+
+// {
+//     let a = 10
+//     let b = 10
+//     let c = a
+
+//     if (a === b){
+//         a += b
+//         b += c
+//     }
+//     else{
+//         a = 0
+//         b = 0
+//     }
+
+//     console.log(`a = ${a} va b = ${b}`)
+// }
